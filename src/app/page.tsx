4b3e38db3e64798899322559f8468b223d118231
Raikyudev <kyuschin@gmail.com>
2025-03-15 17:46:35 +0000

    Refactor homepage and reports UI for improved styling and layout
    Updated the homepage and reports page to enhance visual consistency and usability. Includes changes to layout dimensions, background styles, and color schemes, as well as the introduction of new interactive elements and hover effects. Added new "3 Easy Steps" section to the homepage and integrated a more compact reports table layout for better user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ import { useRouter } from "next/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
   return (
     <div className="">
-        <div className="grid grid-cols-2 w-screen h-screen relative">
+        <div className="grid grid-cols-2 w-screen h-[calc(100vh-70px)] bg-true-black bg-[url('/homepage1.png')] bg-no-repeat bg-[length:60%_auto] bg-left relative">
             {/* Left Column */}
-            <div className="flex flex-col justify-center items-center col-span-1 bg-true-black gap-4 p-10 h-screen">
+            <div className="flex flex-col justify-center items-center col-span-1  gap-4 p-10">
                 <div className="text-5xl text-white">Portman</div>
                 <div className="flex flex-col justify-center items-center mb-4 text-white">
                     <div>Portfolio management</div>
@@ -20,44 +21,43 @@ export default function Home() {
             </div>
 
             {/* Right Column */}
-            <div className="flex flex-col col-span-1 bg-gray justify-center items-center p-10">
-                {/* Text Section - Centered container, but text is left-aligned */}
+            <div className="flex flex-col col-span-1 bg-true-black rounded-bl-[60px] justify-center items-center p-10">
                 <div className="flex flex-col gap-4 w-1/2 text-left">
-                    <div className="text-5xl">Nutmeg. A new approach to investing.</div>
+                    <div className="text-4xl"><span className="text-red">Portman.</span> A new approach to investing.</div>
                     <div className="text-lg">
                         We believe investing should be a clear and straightforward experience. We build and manage diversified portfolios, using technology to keep charges low and clearly show where you’re invested.
                     </div>
                 </div>
-
-                {/* Card Section - Below Text */}
                 <div className="mt-8 flex justify-center w-full">
-                    <Card className="bg-true-black text-white p-6 rounded-lg w-1/2 mt-10">
+                    <Card className="bg-red text-white p-6 rounded-lg no-border w-1/2 mt-10">
                         <CardHeader>
-                            <CardTitle className="text-lg">Wealth Starts Here.</CardTitle>
-                            <CardTitle>Start now.</CardTitle>
+                            <CardTitle className="text-xl text-true-black font">Wealth Starts Here.</CardTitle>
+                            <CardTitle className="text-lg text-white">Start now.</CardTitle>
                         </CardHeader>
                         <CardContent className="flex items-center gap-4">
-                                <Button onClick={() => router.push("/auth/login")}>Log in</Button>
-                                <div>or</div>
-                                <Button onClick={() => router.push("/auth/register")}>Create new account</Button>
+                            <Button className="bg-black text-white"  onClick={() => router.push("/auth/login")}>Log in</Button>
+                            <div>or</div>
+                            <Button className="bg-black text-white" onClick={() => router.push("/auth/register")}>Create new account</Button>
                         </CardContent>
                     </Card>
                 </div>
             </div>
 
             {/* Message Box - Positioned at the bottom */}
-            <div className="absolute bottom-0 left-0 right-0 text-white p-6 text-center">
-                ^ Learn more about Portman below ^
+            <div
+                className="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-red font-bold p-4 mb-6 text-center w-fit rounded-full"
+                style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                ↓ Scroll down to learn more ↓
             </div>
         </div>
 
-        <div className="flex flex-col items-center h-screen">
-            <div className="text-lg p-4 pt-4">Why choose Portman?</div>
+        <div className="flex flex-col items-center">
+            <div className="text-lg text-red font-bold p-4 mt-6">Why choose Portman?</div>
             <div className="p-4 w-3/5">We were the first and we’re now the largest digital wealth manager in the UK with 200k clients. We don’t hide behind complex pricing structures or financial jargon. We don’t keep you in the dark when it comes to your investments or how they’re performing. Instead, we give you complete transparency, and want you to feel empowered.
             </div>
-            <div className="text-lg p-4 mt-4">How Portman Helps you invest</div>
+            <div className="text-lg text-red font-bold p-4 mt-6">How Portman Helps you invest</div>
             <div className="grid grid-cols-3 grid-rows-2 gap-4 w-2/3">
-                <Card className="bg-red">
+                <Card className="bg-true-black hover:bg-red">
                     <CardHeader>
                         <CardTitle>SOME FEATURE</CardTitle>
                     </CardHeader>
@@ -66,7 +66,7 @@ export default function Home() {
                     </CardContent>
                 </Card>
 
-                <Card className="bg-true-black">
+                <Card className="bg-true-black hover:bg-red">
                     <CardHeader>
                         <CardTitle>SOME FEATURE</CardTitle>
                     </CardHeader>
@@ -75,7 +75,7 @@ export default function Home() {
                     </CardContent>
                 </Card>
 
-                <Card className="bg-true-black">
+                <Card className="bg-true-black hover:bg-red">
                     <CardHeader>
                         <CardTitle>SOME FEATURE</CardTitle>
                     </CardHeader>
@@ -84,7 +84,7 @@ export default function Home() {
                     </CardContent>
                 </Card>
 
-                <Card className="bg-true-black">
+                <Card className="bg-true-black hover:bg-red">
                     <CardHeader>
                         <CardTitle>SOME FEATURE</CardTitle>
                     </CardHeader>
@@ -93,7 +93,7 @@ export default function Home() {
                     </CardContent>
                 </Card>
 
-                <Card className="bg-true-black">
+                <Card className="bg-true-black hover:bg-red">
                     <CardHeader>
                         <CardTitle>SOME FEATURE</CardTitle>
                     </CardHeader>
@@ -102,7 +102,7 @@ export default function Home() {
                     </CardContent>
                 </Card>
 
-                <Card className="bg-true-black">
+                <Card className="bg-true-black hover:bg-red">
                     <CardHeader>
                         <CardTitle>SOME FEATURE</CardTitle>
                     </CardHeader>
@@ -110,16 +110,40 @@ export default function Home() {
                         DESCRRIPTION
                     </CardContent>
                 </Card>
+            </div>
+            <div className="text-lg text-red font-bold p-4 mt-6">3 Easy steps to get you started</div>
+            <div className="grid grid-cols-3 grid-rows-1 gap-4 w-2/3 mb-16">
+                <Card className="bg-true-black hover:border-red">
+                    <CardHeader>
+                        <CardTitle className="text-lg">Step 1</CardTitle>
+                    </CardHeader>
+                    <CardContent className="h-auto">
+                        Log in or create a new account
+                        <Image src="/homepage1.png" alt="Description of image" className="w-85 h-auto" />
+                    </CardContent>
+                </Card>
 
+                <Card className="bg-true-black hover:border-red">
+                    <CardHeader>
+                        <CardTitle className="text-lg">Step 2</CardTitle>
+                    </CardHeader>
+                    <CardContent className="h-auto">
+                        Create your first portfolio and add your transactions
+                        <Image src="/homepage1.png" alt="Description of image" className="w-85 h-auto" />
+                    </CardContent>
+                </Card>
 
-
+                <Card className="bg-true-black hover:border-red">
+                    <CardHeader>
+                        <CardTitle className="text-lg">Step 3</CardTitle>
+                    </CardHeader>
+                    <CardContent className="h-auto">
+                        Explore your insights and see how your portfolio is performing
+                        <Image src="/homepage1.png" alt="Description of image" className="w-85 h-auto" />
+                    </CardContent>
+                </Card>
             </div>
         </div>
-      <h1>Portman</h1>
-      <div>
-        <button onClick={() => router.push("/auth/login")}>Login</button>
-        <button onClick={() => router.push("/auth/register")}>Register</button>
-      </div>
     </div>
   );
 }
--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -31,59 +30,53 @@ export default function Page() {
   });
 
   return (
-    <ProtectedLayout>
-      <div className="p-6">
-        <div className="max-w-4xl mx-auto w-[90%]">
-          <Card className="bg-background w-full">
-            <CardHeader>
-              <CardTitle className="text-2xl font-bold">
-                User’s Reports
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              {/* Date Filters and Sort */}
-              <div className="flex justify-end mb-4 space-x-4">
-                <div className="flex items-center space-x-2">
-                  <label className="text-sm">From</label>
-                  <Input
-                    type="date"
-                    value={fromDate}
-                    onChange={(e) => setFromDate(e.target.value)}
-                    className="bg-background border-border text-foreground"
-                  />
-                </div>
-                <div className="flex items-center space-x-2">
-                  <label className="text-sm">To</label>
-                  <Input
-                    type="date"
-                    value={toDate}
-                    onChange={(e) => setToDate(e.target.value)}
-                    className="bg-background border-border text-foreground"
-                  />
-                </div>
-                <button className="border border-border rounded-md px-4 py-2 text-sm">
-                  Sort
-                </button>
-              </div>
-              <ScrollArea className="h-[30vh] w-full rounded-xl">
-                {/* Table of Reports */}
-                <Table
+      <ProtectedLayout>
+        <div className="flex flex-col container mx-auto gap-4 ">
+
+          <div className="flex justify-end mb-4 space-x-4 bg-true-black p-4 rounded-lg">
+            <h1 className="text-2xl font-bold">User&apos;s Reports</h1>
+            <div className="flex items-center space-x-2">
+              <label className="text-sm">From</label>
+              <Input
+                  type="date"
+                  value={fromDate}
+                  onChange={(e) => setFromDate(e.target.value)}
+                  className="bg-background border-border text-foreground"
+              />
+            </div>
+            <div className="flex items-center space-x-2">
+              <label className="text-sm">To</label>
+              <Input
+                  type="date"
+                  value={toDate}
+                  onChange={(e) => setToDate(e.target.value)}
+                  className="bg-background border-border text-foreground"
+              />
+            </div>
+            <button className="bg-red rounded-md px-4 py-2 text-sm">
+              Sort
+            </button>
+          </div>
+          <div className="p-4 bg-true-black rounded-xl">
+            <ScrollArea className="h-[60vh] w-full rounded-lg">
+              {/* Table of Reports */}
+              <Table
                   className="bg-true-black rounded-xl border-separate"
                   style={{ borderSpacing: "0 8px" }}
-                >
-                  <TableHeader>
-                    <TableRow className="border-b border-gray hover:bg-true-black">
-                      <TableHead className="px-4 py-2">Name</TableHead>
-                      <TableHead className="px-4 py-2">Type</TableHead>
-                      <TableHead className="px-4 py-2">Date Created</TableHead>
-                      <TableHead className="px-4 py-2">Time Period</TableHead>
-                      <TableHead className="px-4 py-2">Format</TableHead>
-                      <TableHead className="px-4 py-2">Size</TableHead>
-                      <TableHead className="px-4 py-2">Download</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {reports.map((report, index) => (
+              >
+                <TableHeader>
+                  <TableRow className="border-b border-gray hover:bg-true-black">
+                    <TableHead className="px-4 py-2">Name</TableHead>
+                    <TableHead className="px-4 py-2">Type</TableHead>
+                    <TableHead className="px-4 py-2">Date Created</TableHead>
+                    <TableHead className="px-4 py-2">Time Period</TableHead>
+                    <TableHead className="px-4 py-2">Format</TableHead>
+                    <TableHead className="px-4 py-2">Size</TableHead>
+                    <TableHead className="px-4 py-2">Download</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {reports.map((report, index) => (
                       <TableRow key={index} className="hover:bg-transparent">
                         <TableCell className="px-4 py-2 border-b border-gray w-1/7">
                           {report.name}
@@ -106,40 +99,39 @@ export default function Page() {
                         <TableCell className="px-4 py-2 border-b border-gray w-1/7">
                           <button>
                             <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-5 w-5"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
                             >
                               <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth={2}
+                                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
                               />
                             </svg>
                           </button>
                         </TableCell>
                       </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-                <ScrollBar orientation="horizontal" />
-              </ScrollArea>
-              {/* Generate Report Button */}
-              <div className="mt-6 flex justify-center">
-                <Button
+                  ))}
+                </TableBody>
+              </Table>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+            {/* Generate Report Button */}
+            <div className="mt-6 mb-4 flex justify-center">
+              <Button
                   asChild
-                  className="bg-red-500 hover:bg-red-600 text-white font-semibold py-3 px-6 rounded-lg"
-                >
-                  <Link href="/reports/generate">Generate a Report</Link>
-                </Button>
-              </div>
-            </CardContent>
-          </Card>
+                  className="bg-red text-white font-semibold py-3 px-6 rounded-lg"
+              >
+                <Link href="/reports/generate">Generate a Report</Link>
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </ProtectedLayout>
+
+      </ProtectedLayout>
   );
 }
